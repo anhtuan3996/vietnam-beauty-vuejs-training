1ba1e8b052a4e7a8dc5d71ec5827e795d86ef9c5
Tuanca7175 <tuanca7175@co-well.com.vn>
2020-05-14 10:20:15 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">
+        <img src="./assets/images/vietnam.jpg" alt="">
+      </div>
+      <div class="section">
+        <img src="./assets/images/vietnam2.jpg" alt="">
+      </div>
+    </full-page>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        options: {
+          licenseKey: 'JDF8XRD6-JDF8XRD6-JDF8XRD6-JDF8XRD6',
+          sectionsColor: ['#41b883', '#ff5f45', '#0798ec']
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueFullPage from 'vue-fullpage.js'
+
+Vue.use(VueFullPage);
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
